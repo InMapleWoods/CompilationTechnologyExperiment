81606ceb0325c35505b5671313e568c51557f718
as6624504 <734731953@qq.com>
2019-12-23 17:05:12 +0800

    12/23 递归下降分析program

--- CompilationTechnologyExperiment/SyntaxAnalysis.cs
+++ CompilationTechnologyExperiment/SyntaxAnalysis.cs
@@ -9,9 +9,36 @@ namespace CompilationTechnologyExperiment
     
     class SyntaxAnalysis
     {
+        class token
+        {
+            int label;
+            string name;
+            int code;
+            int addr;
+            int getlabel() { return label; }
+            string getname() { return name;}
+            int getcode() { return code; }
+            int getaddr() { return addr; }
+            token() { label = 0;code = 0;addr = 0; }
+        }
+        Boolean SyntaxAnalaysisResult()
+        {
+            getbuf();
+            program();
+            Console.WriteLine("YES\n");
+            return (true);
+        }
+       int i = 0;
+       token t = new token();
        void getbuf()
         {
-
+            
+        }
+        Boolean program()
+        {
+            proghead();
+            block();
+            if()
         }
     }
 }
